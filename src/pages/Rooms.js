d97d20506c58f09577bdb3a73a29574a879529ab
Arun Awasthi <arunawasthi005@gmail.com>
2025-08-20 18:02:17 +0530

    image fix

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -13,7 +13,11 @@ export default function Rooms() {
       <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
         {rooms.map((room) => (
           <div key={room.id} className="p-5 border rounded-lg shadow hover:shadow-lg">
-            <img src='elijah-pilchard-PkfZ6k0Gozo-unsplash.jpg' alt={room.name} className="rounded mb-3"/>
+            <img
+              src={`${process.env.PUBLIC_URL}/images/elijah-pilchard-PkfZ6k0Gozo-unsplash.jpg`}
+              alt={room.name}
+              className="rounded mb-3"
+            />
             <h3 className="text-xl font-semibold">{room.name}</h3>
             <p>Price: ₹{room.price}/month</p>
             <p>Location: {room.location}</p>
